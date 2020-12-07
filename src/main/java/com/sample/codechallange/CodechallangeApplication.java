3e55f3f5132f90fac45e0fb47daf0de4baa22d1c
Sujan Parajuli <parajulisujan360@gmail.com>
2020-12-07 14:45:00 -0600

    First commit

---
+++ src/main/java/com/sample/codechallange/CodechallangeApplication.java
@@ -0,0 +1,13 @@
+package com.sample.codechallange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodechallangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodechallangeApplication.class, args);
+	}
+
+}
